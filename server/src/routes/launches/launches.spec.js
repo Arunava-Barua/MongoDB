8944106a03308898427b2c6509b4694ab65c1ae7
Arunava-Barua <rahulbarua31@gmail.com>
2022-10-05 12:05:37 +0530

    Continuous test ingeration with mongoose

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -1,41 +1,52 @@
 const app = require("../../app.js");
 const request = require("supertest");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
-describe("GET /launches endpoint", () => {
-  test("It should return 200 success", async () => {
-    await request(app)
-      .get("/launches")
-      .expect("Content-Type", /json/)
-      .expect(200);
+describe("Launches API", () => {
+  beforeAll(async () => {
+    await mongoConnect();
   });
-});
 
-describe("POST /launches endpoint", () => {
-  const completeLaunchData = {
-    mission: "Arv Travels",
-    rocket: "NCC 1701-D",
-    target: "Kepler-186 f",
-    launchDate: "January 4, 2030",
-  }
+  afterAll(async () => {
+    await mongoDisconnect();
+  });
 
-  const launchDataWithoutDate = {
-    mission: "Arv Travels",
-    rocket: "NCC 1701-D",
-    target: "Kepler-186 f",
-  }
+  describe("GET /launches endpoint", () => {
+    test("It should return 200 success", async () => {
+      await request(app)
+        .get("/launches")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+  });
 
-  test("It should return 201", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(completeLaunchData)
-      .expect("Content-Type", /json/)
-      .expect(201);
+  describe("POST /launches endpoint", () => {
+    const completeLaunchData = {
+      mission: "Arv Travels",
+      rocket: "NCC 1701-D",
+      target: "Kepler-62 f",
+      launchDate: "January 4, 2030",
+    };
+
+    const launchDataWithoutDate = {
+      mission: "Arv Travels",
+      rocket: "NCC 1701-D",
+      target: "Kepler-62 f",
+    };
+
+    test("It should return 201", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(completeLaunchData)
+        .expect("Content-Type", /json/)
+        .expect(201);
 
       const requestDate = new Date(completeLaunchData.launchDate).valueOf();
       const responseDate = new Date(response.body.launchDate).valueOf();
 
-
-      expect(responseDate).toBe(requestDate);f
+      expect(responseDate).toBe(requestDate);
+      
       expect(response.body).toMatchObject(launchDataWithoutDate);
+    });
   });
 });
